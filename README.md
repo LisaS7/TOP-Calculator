cdd169657658ef046edf933e0cece52a850dc75b
Lisa Linux <mclisa00@gmail.com>
2022-07-08 11:11:59 +0100

    Functionality mostly working, more testing needed.

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ Calculator exercise from [The Odin Project](https://www.theodinproject.com/lesso
 [Live Demo](https://lisas7.github.io/TOP-Calculator/)
 
 What I learned:
-* Using the calc() function in CSS
 * Creating a grid with flexbox
+* HTML output element
+* Using the calc() function in CSS (subsequently removed as it was messing up the display border)
--- main.js
+++ main.js
@@ -1,3 +1,30 @@
+const outputDisplay = document.getElementsByTagName('output').display;
+const previousDisplay = document.getElementsByTagName('output').previous;
+let result = null;
+let operation = null;
+
+
+// BUTTON EVENTS
+document.getElementById('clear').addEventListener('click', clearAll);
+
+document.querySelectorAll('[data-type="number"]').forEach(item => {
+    item.addEventListener('click', () => {setDisplay(item.value)});
+});
+
+document.querySelectorAll('[data-type="operator"]').forEach(item => {
+    item.addEventListener('click', () => {
+        setOperation(item.value);
+        displayPrevious(result, operation);
+    });
+});
+
+document.getElementById('equals').addEventListener('click', () => {
+    equals();
+    displayPrevious(result);
+});
+
+
+// MATHS FUNCTIONS
 function add(a, b) {
     return a + b;
 }
@@ -24,6 +51,40 @@ function operate(operator, a, b) {
             return divide(a, b);
         case '*':
             return multiply(a, b);
+    }     
+}
+
+function equals() {
+    result = operate(operation, result, getDisplay())
+}
+
+function setOperation(operator) {
+    if (result) {
+        equals();
+    } else {
+        result = getDisplay();
     }
-        
+    operation = operator;
+}
+
+
+// CONTROL DISPLAY
+function getDisplay() {
+    return Number(outputDisplay.textContent);
+}
+
+function setDisplay(value) {
+    outputDisplay.textContent += value;
+}
+
+function displayPrevious(number, operation='') {
+    previousDisplay.textContent = `${number} ${operation}`;
+    outputDisplay.textContent = '';
+}
+
+function clearAll() {
+    outputDisplay.textContent = '';
+    previousDisplay.textContent = '';
+    result = null;
+    operation = null;
 }
